000a792638154c0f9c15688de29402d747b7b993
krish <krishnendusarathkumar@gmail.com>
2026-05-29 21:05:18 +0530

    Fix CORS for production

--- backend/index.js
+++ backend/index.js
@@ -8,7 +8,8 @@ const app = express();
 app.use(express.json());
 app.use(cookieParser());
 app.use(cors({
-  origin: 'http://localhost:5173',
+  // origin: 'http://localhost:5173',
+   origin: ['http://localhost:5173', 'https://leaf-mark.vercel.app'],
   credentials: true
 }));
 
